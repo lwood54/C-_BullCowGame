a341b8ccaf3a27455b132fad893b1fd772b7677d
Logan Wood <lwood3499@gmail.com>
2018-05-05 20:31:56 -0500

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Logan Wood on 5/5/18.
+//  Copyright © 2018 Logan Wood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
